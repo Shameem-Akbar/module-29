a732bf9f8797a01d65919786fd27285d59e101ae
Shameem-Akbar <Shameemakbar789@gmail.com>
2023-02-16 02:25:03 +0600

    finallll

---
+++ js/red-cross.js
@@ -0,0 +1,16 @@
+document.getElementById('red-cross').addEventListener('click', function () {
+    document.getElementById('case-cart').style.display = 'none';
+
+
+
+    //MISTAKE
+    // const caseNumberField = document.getElementById('case-number-field');
+    // const caseNUmberString = caseNumberField.value;
+    // const previousCaseNumber = parseInt(caseNUmberString);
+    // let newCaseNumber;
+    // if (previousCaseNumber > 0) {
+    //     newCaseNumber = 0;
+    // }
+    // caseNumberField.value = newCaseNumber;
+    // return newCaseNumber;
+})
